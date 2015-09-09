11d298c62d718075929c229babc1f380cd033fcd
강경완 <lk@lemonlab.co.kr>
2015-09-10 02:22:59 +0900

    머터리얼 디자인 추가

--- app/src/main/java/com/lemonlab/wordbook/MainActivity.java
+++ app/src/main/java/com/lemonlab/wordbook/MainActivity.java
@@ -1,17 +1,50 @@
 package com.lemonlab.wordbook;
 
-import android.support.v7.app.ActionBarActivity;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawer = (DrawerLayout) findViewById(R.id.drawer);
+        NavigationView nv = (NavigationView) findViewById(R.id.navigation_view);
+        nv.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        if (menuItem.isCheckable()) {
+                            menuItem.setChecked(true);
+                        }
+                        Toast.makeText(getApplicationContext(),
+                                menuItem.getTitle(), Toast.LENGTH_SHORT).show();
+                        drawer.closeDrawers();
+                        return true;
+                    }
+                });
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar ab = getSupportActionBar();
+        if (null != ab) {
+            ab.setHomeAsUpIndicator(R.drawable.ic_menu);
+            ab.setDisplayHomeAsUpEnabled(true);
+        }
     }
 
     @Override
@@ -31,7 +64,11 @@ public class MainActivity extends ActionBarActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
+        }else if(id == android.R.id.home){
+            drawer.openDrawer(GravityCompat.START);
+            return true;
         }
+
 
         return super.onOptionsItemSelected(item);
     }
